e5c2ea2be50e743f4a668a6a5a586d592361b991
Jordyn Battle <jbattle@pcp072066pcs.wireless.calpoly.edu>
2014-04-17 15:41:05 -0700

    Added compile.sh and run.sh

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -Xlint:unchecked -d ./classfiles -cp .:./lib/twitter4j-4.0.1/lib/twitter4j-core-4.0.1.jar src/*.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp classfiles:./lib/twitter4j-4.0.1/lib/twitter4j-core-4.0.1.jar Twitter4jTest
